b546628cdc87cd7a635c5c449bbe9ff735456085
CannibalVox <sdbaynham@gmail.com>
2013-11-09 20:32:38 -0600

    Params going to minecraft now start with a double -

--- src/main/java/net/technicpack/legacywrapper/StartupParameters.java
+++ src/main/java/net/technicpack/legacywrapper/StartupParameters.java
@@ -58,16 +58,16 @@ public final class StartupParameters {
     @Parameter(names = {"--assetsDir"}, description = "Assets directory")
     private String assetsDir;
 
-    @Parameter(names = {"-width"}, description = "Sets the width of the minecraft window to be fixed to this.")
+    @Parameter(names = {"--width"}, description = "Sets the width of the minecraft window to be fixed to this.")
     private int width = -1;
 
-    @Parameter(names = {"-height"}, description = "Sets the height of the minecraft window to be fixed to this.")
+    @Parameter(names = {"--height"}, description = "Sets the height of the minecraft window to be fixed to this.")
     private int height = -1;
 
-	@Parameter(names = {"-title"}, description = "The title bar text for the Minecraft frame.")
+	@Parameter(names = {"--title"}, description = "The title bar text for the Minecraft frame.")
 	private String title = "Minecraft";
 
-	@Parameter(names = {"-icon"}, description = "The title bar icon for the Minecraft frame.")
+	@Parameter(names = {"--icon"}, description = "The title bar icon for the Minecraft frame.")
 	private String icon = null;
 
     public String getAuthPlayerName() { return auth_player_name; }
